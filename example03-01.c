1f4359dfc81f7d520ff85201b88dfc556990484f
larryback <41230266+larryback@users.noreply.github.com>
2019-05-15 01:02:44 +0900

    5월15일새벽 첫 commit

---
+++ example03-01.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("%c\n", 'K');
+	printf("%c\n", 65);
+	printf("%d\n", 12);
+	printf("%i\n", 12);
+	printf("%o\n", 12);
+	printf("%u\n", 12);
+	printf("%x\n", 27);
+	printf("%X\n", 27);
+	printf("%f\n", 12.34);
+	printf("%5.2f\n", 12.34);
+	printf("%e\n", 123.45);
+	printf("%E\n", 123.45);
+	printf("%s\n", "kim");
+	printf("%p\n", "kim");
+
+	return 0;
+}
---
+++ example03-02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+	char ch;
+	int in;
+	ch=in='a';
+	printf("ch=%c\n", ch);
+	printf("ch=%d\n", ch);
+	ch=in=99;
+	printf("ch=%d\n", in);
+	printf("ch=%c\n", in);
+	
+	
+}
