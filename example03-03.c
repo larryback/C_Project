f2fb0ba54dc0019eb6c80e083cd9fbc21f0615ff
larryback <41230266+larryback@users.noreply.github.com>
2019-05-15 23:04:06 +0900

    5월15일저녁집

---
+++ example03-03.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+void main()
+
+{
+	printf("|%d|\n\n", 1234);
+	printf("|%6d|\n\n", 1234);
+	printf("|%-6d|\n\n", 1234);
+	printf("|%06d|\n\n", 1234);
+	printf("|%6f|\n\n", 12.345);
+	printf("|%6.1f|\n\n", 12.345);
+	printf("|%06.2f|\n\n", 12.345);
+	
+	
+	
+	
+	
+}
---
+++ example03-04.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+
+{
+	int x1, y1;
+	float x2;
+	double y2;
+	
+	printf("\n두개의 정수를 입력하세요: ");
+	scanf("%d %d", &x1, &y1);
+	printf("입력된 정수 %d 와 %d의 합은 %d입니다\n", x1,y1,x1+y1);
+	printf("\n두개의 실수를 입력하세요: ");
+	scanf("%f %lf", &x2,&y2);
+	printf("입력된 실수 %f 와 %f의 합은 %f입니다\n", x2,y2,x2+y2);
+	
+	
+	
+	
+	
+	
+}
